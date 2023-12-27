fa1e9fc1955153c56600d2ae69851faca28d1401
Mahmoud Khamis Ahmed <152819907+Mahmoud-Khamis-Claro@users.noreply.github.com>
2023-12-27 12:02:35 +0200

    refactor dashboard players ranked by score

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,29 +1,37 @@
 let playersArr = [];
 $(async function () {
-  $(".results-container").empty();
+  $('.results-container').empty();
   const playersData = await loadFromFirebase();
   for (const playerId in playersData) {
+    playersData[playerId].id = playerId;
     playersArr.push(playersData[playerId]);
-    const tr = $("<tr/>", {
-      id: playerId,
+  }
+
+  playersArr.sort(function (a, b) {
+    return b.score - a.score;
+  });
+
+  for (let i = 0; i < playersArr.length; i++) {
+    const tr = $('<tr/>', {
+      id: playersArr[i].id,
     }).html(`
-    <td>${playersData[playerId].name}</td>
-    <td>${playersData[playerId].email}</td>
-    <td>${playersData[playerId].score}</td>
+    <td>${playersArr[i].name}</td>
+    <td>${playersArr[i].email}</td>
+    <td>${playersArr[i].score}</td>
     <td class="text-center">
-      <button class="btn btn-danger" onclick="deletePlayer('${playerId}')">delete</button>
+      <button class="btn btn-danger" onclick="deletePlayer('${playersArr[i].id}')">delete</button>
     </td>
     `);
-    $(".results-container").append(tr);
+    $('.results-container').append(tr);
   }
 });
 
 function download() {
-  filename = "Game_Servier_LDL v2_report.xlsx";
+  filename = 'Game_Biogen - Ignite Me Game_report.xlsx';
   const ws = XLSX.utils.json_to_sheet(playersArr);
   var wb = XLSX.utils.book_new();
 
   /* Export to file (start a download) */
-  XLSX.utils.book_append_sheet(wb, ws, "players");
+  XLSX.utils.book_append_sheet(wb, ws, 'players');
   XLSX.writeFile(wb, filename);
 }
